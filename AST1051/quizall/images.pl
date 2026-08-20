4e6378559e9df4e33c95e0a3db954efa8f4e6158
Jason Pinkney <j-pinkney@onu.edu>
2026-08-20 11:19:41 -0400

    Imported AST1051 export others

---
+++ AST1051/quizall/images.pl
@@ -0,0 +1,36 @@
+# LaTeX2HTML 2002 (1.62)
+# Associate images original text with physical files.
+
+
+$key = q/^3;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="21" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$^3$">|; 
+
+$key = q/^{o};MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="12" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$^{o}$">|; 
+
+$key = q/^{o}45';MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="35" HEIGHT="19" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$^{o} 45'$">|; 
+
+$key = q/'';MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="13" HEIGHT="20" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$''$">|; 
+
+$key = q/9^h55^m;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="54" HEIGHT="20" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$9^h 55^m$">|; 
+
+1;
+
